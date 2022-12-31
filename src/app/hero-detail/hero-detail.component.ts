b15d81034c8d23c785de6d2349c13b4b490aea92
joseph <josephsherpa7@gmail.com>
2022-12-31 14:31:16 -0800

    toursfheroes

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Hero } from '../hero';
+import {  Component,Input } from '@angular/core';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+
+
+  @Input() hero?: Hero;
+}
